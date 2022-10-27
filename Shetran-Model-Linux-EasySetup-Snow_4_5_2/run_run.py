b038c844616ec8c6025f1431099d8c9a395d511c
Robin-Wardle <robin.wardle@newcastle.ac.uk>
2022-10-27 16:50:49 +0100

    Added SHETRAN run and release actions for DAFNI deployment

---
+++ Shetran-Model-Linux-EasySetup-Snow_4_5_2/run_run.py
@@ -0,0 +1,71 @@
+import subprocess
+import shutil
+from glob import glob
+import os
+import datetime
+
+data_path = os.getenv('DATA_PATH', '/data')
+
+inputs = os.path.join(data_path, 'inputs')
+outputs = os.path.join(data_path, 'outputs')
+
+if os.path.exists(outputs):
+    shutil.rmtree(outputs)
+
+os.mkdir(outputs)
+
+run_path = os.path.join(outputs, 'shetran')
+
+shutil.copytree(inputs, run_path)
+
+try:
+    library = glob(os.path.join(run_path, '*.txt'))[0]
+except IndexError:
+    raise Exception('Library file missing')
+
+
+subprocess.call(['./Shetran-Linux', library])
+# subprocess.call(['./shetran-linux', '-f', glob(os.path.join(run_path, 'rundata_*'))[0]])
+
+title = os.getenv('TITLE', 'SHETran run')
+description = 'SHETRAN simulaiton.'
+geojson = {}
+
+
+metadata = f"""{{
+  "@context": ["metadata-v1"],
+  "@type": "dcat:Dataset",
+  "dct:language": "en",
+  "dct:title": "{title}",
+  "dct:description": "{description}",
+  "dcat:keyword": [
+    "shetran"
+  ],
+  "dct:subject": "Environment",
+  "dct:license": {{
+    "@type": "LicenseDocument",
+    "@id": "https://creativecommons.org/licences/by/4.0/",
+    "rdfs:label": null
+  }},
+  "dct:creator": [{{"@type": "foaf:Organization"}}],
+  "dcat:contactPoint": {{
+    "@type": "vcard:Organization",
+    "vcard:fn": "DAFNI",
+    "vcard:hasEmail": "support@dafni.ac.uk"
+  }},
+  "dct:created": "{datetime.datetime.now().isoformat()}Z",
+  "dct:PeriodOfTime": {{
+    "type": "dct:PeriodOfTime",
+    "time:hasBeginning": null,
+    "time:hasEnd": null
+  }},
+  "dafni_version_note": "created",
+  "dct:spatial": {{
+    "@type": "dct:Location",
+    "rdfs:label": null
+  }},
+  "geojson": {geojson}
+}}
+"""
+with open(os.path.join(outputs, 'metadata.json'), 'w') as f:
+    f.write(metadata)
